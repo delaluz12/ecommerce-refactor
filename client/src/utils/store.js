50b68fcaf666b82cff06c3991c6b5ed3b6448ae3
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-23 22:07:19 -0600

    added store redux component

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -3,5 +3,8 @@ import {createStore} from 'redux';
 
 import reducers from './reducers';
 
+const store = createStore(reducers)
 
-export default createStore(reducers)
+// export default createStore(reducers)
+
+export default store; 
